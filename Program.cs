3822c5a3e33eeb7fd943ecfb09f30923dbf7cdf0
Vivian Lu <97366023+lu279463768@users.noreply.github.com>
2023-07-11 20:22:17 -0400

    BlazzingPizza1

--- Program.cs
+++ Program.cs
@@ -1,43 +1,51 @@
-using BlazingPizza;
-using BlazingPizza.Extensions;
-using Microsoft.EntityFrameworkCore;
+//This statement allows the app to use the new service.
+using BlazingPizza.Data;
+using BlazingPizza.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+//initialized the PizzaStoreContext
+// The first AddHttpClient statement allows the app to access HTTP commands. The app uses an HttpClient to get the JSON for pizza specials.
+builder.Services.AddHttpClient();
+// The second statement registers the new PizzaStoreContext and provides the filename for the SQLite database.
+builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddHttpClient();
-builder.Services.AddDbContext<PizzaStoreContext>(options =>
-    options.UseSqlite("Data Source=pizza.db"));
+
 builder.Services.AddScoped<OrderState>();
 
+
 var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapRazorPages();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+
 
 // Initialize the database
 var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
 using (var scope = scopeFactory.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
-    if (await db.Database.EnsureCreatedAsync())
+    if (db.Database.EnsureCreated())
     {
-        await SeedData.InitializeAsync(db);
+        SeedData.Initialize(db);
     }
 }
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.MapPizzaApi();
-
+//This change creates a database scope with the PizzaStoreContext. If there isn't a database already created,
+//it calls the SeedData static class to create one.
 app.Run();
